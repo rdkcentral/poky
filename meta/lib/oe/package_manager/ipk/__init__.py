e3a577bfb1c811b07517daf04b648f4ac3af46ba
Sreejith Ravi <sreejith.ravi@sky.uk>
2026-02-17 11:54:31 +0000

    RDKE-1036 : Updated with review comment

--- meta/lib/oe/package_manager/ipk/__init__.py
+++ meta/lib/oe/package_manager/ipk/__init__.py
@@ -180,7 +180,8 @@ class OpkgPM(OpkgDpkgPM):
         self.saved_opkg_dir = self.d.expand('${T}/saved/%s' % self.task_name)
         if not os.path.exists(self.d.expand('${T}/saved')):
             bb.utils.mkdirhier(self.d.expand('${T}/saved'))
-
+        # custom feed config is only required for targets which is configured through IPK_FEED_URIS.
+        # IPKGCONF_SDK is for host feeds which are not configured through IPK_FEED_URIS.
         self.from_feeds = (self.d.getVar('BUILD_IMAGES_FROM_FEEDS') or "") == "1" and not self.config_file == d.getVar("IPKGCONF_SDK")
         if self.from_feeds:
             if bb.data.inherits_class('custom-rootfs-creation', d):
